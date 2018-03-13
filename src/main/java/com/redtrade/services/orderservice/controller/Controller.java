a108812ce7e8a37a1db4cc86d798488201956e08
Caelan Evans <evans.caelan@gmail.com>
2018-03-12 21:37:55 -0500

    Test adding order

--- src/main/java/com/redtrade/services/orderservice/controller/Controller.java
+++ src/main/java/com/redtrade/services/orderservice/controller/Controller.java
@@ -1,8 +1,10 @@
 package com.redtrade.services.orderservice.controller;
 
+import com.redtrade.services.orderservice.entity.Order;
+import com.redtrade.services.orderservice.repository.OrderRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,9 +12,22 @@ import org.springframework.web.bind.annotation.RestController;
 public class Controller {
     private static final Logger LOGGER = LoggerFactory.getLogger(Controller.class);
 
+    @Autowired
+    private OrderRepository orderRepository;
+
+//    @RequestMapping("/")
+//    public String ping() {
+//        LOGGER.info("Received ping");
+//        return "order pong";
+//    }
+
     @RequestMapping("/")
-    public String ping() {
-        LOGGER.info("Received ping");
-        return "order pong";
+    public String order() {
+        LOGGER.info("Received order");
+        Order order = new Order(Order.Type.BUY, "AAPL", 100.0);
+        orderRepository.save(order);
+        LOGGER.info("Order submitted");
+        LOGGER.info("All orders: {}", orderRepository.findAll());
+        return "OK";
     }
 }
